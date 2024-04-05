637fdd358eb06b54a0b00e3e122175ffa39efe7f
mbunch_kascope <michael8818bunch@gmail.com>
2024-04-05 00:17:20 -0400

    Effect Classes created, half the cards complete

---
+++ Assets/CardEffect/LM/Green/SymbareAngoramon_LM_011.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DCGO.CardEffects
+{
+    public class SymbareAngoramon_LM_011 : CEntity_Effect
+    {
+        public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
+        {
+            List<ICardEffect> cardEffects = new List<ICardEffect>();
+
+
+
+            return cardEffects;
+        }
+    }
+}
