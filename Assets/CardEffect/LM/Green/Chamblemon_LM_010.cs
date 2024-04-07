dcc32cb929931320d1e9260ea3cbef878d36a1d4
mbunch_kascope <michael8818bunch@gmail.com>
2024-04-06 21:53:08 -0400

    Final green cards

--- Assets/CardEffect/LM/Green/Chamblemon_LM_010.cs
+++ Assets/CardEffect/LM/Green/Chamblemon_LM_010.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace DCGO.CardEffects
+namespace DCGO.CardEffects.LM
 {
     public class Chamblemon_LM_010 : CEntity_Effect
     {
