8bbfaba59f50b237628377e68cdb494e1e85a89b
mbunch_kascope <michael8818bunch@gmail.com>
2024-04-06 22:34:56 -0400

    Initial commit, publimon created

---
+++ Assets/CardEffect/LM/Yellow/Publimon_LM_007.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DCGO.CardEffects.LM
+{
+    public class Publimon_LM_007 : CEntity_Effect
+    {
+        public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
+        {
+            List<ICardEffect> cardEffects = new List<ICardEffect>();
+
+            #region End Of Attack
+            if(timing == EffectTiming.OnEndAttack)
+            {
+                ActivateClass activateClass = new ActivateClass();
+                activateClass.SetUpICardEffect("Place top card of this Digimon to security", CanUseCondition, card);
+                activateClass.SetUpActivateClass(CanActivateCondition, ActivateCoroutine, -1, false, EffectDiscription());
+                cardEffects.Add(activateClass);
+
+                string EffectDiscription()
+                {
+                    return "[End of Attack] Place this Digimon on top of your security stack.";
+                }
+
+                bool CanUseCondition(Hashtable hashtable)
+                {
+                    if (CardEffectCommons.IsExistOnBattleArea(card))
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                bool CanActivateCondition(Hashtable hashtable)
+                {
+                    if (CardEffectCommons.IsExistOnBattleArea(card))
+                    {
+                        if (card.Owner.CanAddSecurity(activateClass))
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+
+                IEnumerator ActivateCoroutine(Hashtable _hashtable)
+                {
+                    if (CardEffectCommons.IsExistOnBattleArea(card))
+                    {
+                        yield return ContinuousController.instance.StartCoroutine(GManager.instance.GetComponent<Effects>().CreateDebuffEffect(card.PermanentOfThisCard()));
+
+                        CardSource cardSource = card.PermanentOfThisCard().TopCard;
+                        Permanent permanent = card.PermanentOfThisCard();
+
+                        yield return ContinuousController.instance.StartCoroutine(CardObjectController.RemoveFromAllArea(cardSource));
+
+                        permanent.ShowingPermanentCard.ShowPermanentData(true);
+
+                        yield return ContinuousController.instance.StartCoroutine(GManager.instance.GetComponent<Effects>().RemoveDigivolveRootEffect(cardSource, permanent));
+
+                        if (!cardSource.IsToken)
+                        {
+                            yield return ContinuousController.instance.StartCoroutine(CardObjectController.AddSecurityCard(cardSource));
+
+                            yield return ContinuousController.instance.StartCoroutine(GManager.instance.GetComponent<Effects>().CreateRecoveryEffect(cardSource.Owner));
+
+                            yield return ContinuousController.instance.StartCoroutine(new IAddSecurity(cardSource.Owner).AddSecurity());
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            //Security Effect
+            if (timing == EffectTiming.SecuritySkill)
+            {
+                cardEffects.Add(CardEffectFactory.PlaySelfDigimonAfterBattleSecurityEffect(card: card));
+            }
+
+            return cardEffects;
+        }
+    }
+}
---
+++ Assets/CardEffect/LM/Yellow/Quantumon_LM_020.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DCGO.CardEffects.LM
+{
+    public class Quantumon_LM_020 : CEntity_Effect
+    {
+        public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
+        {
+            List<ICardEffect> cardEffects = new List<ICardEffect>();
+
+            #region When Digivolving
+
+            #endregion
+
+            #region Start of Opponent's Turn
+
+            #endregion
+
+            return cardEffects;
+        }
+    }
+}
