7fc16c7283f1494bb204e3559f05051d6f87c80f
mbunch_kascope <michael8818bunch@gmail.com>
2024-04-10 16:04:27 -0400

    Initial Creation of Partition keyword

--- Assets/Scripts/DataBase.cs
+++ Assets/Scripts/DataBase.cs
@@ -501,6 +501,11 @@ public class DataBase : MonoBehaviour
         return "<Alliance> (When this Digimon attacks, by suspending 1 of your other Digimon, this Digimon adds the suspended Digimon's DP and gains <Security Attack +1> for the attack.)";
     }
 
+    public static string PartitionEffectDiscription()
+    {
+        return "<Partition> (When this Digimon with 1 of each specified card in its digivolution cards would leave the battle area other than by one of your effects or in battle, you may play 1 of each card without paying their costs)";
+    }
+
     public static string ReplaceToASCII(string text)
     {
         if (string.IsNullOrEmpty(text))
