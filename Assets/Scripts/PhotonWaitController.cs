7ff98dc57dd0306c560c8d4b27bc9c9b40870370
cchmc-burkewm <William.Burke@cchmc.org>
2024-05-21 16:21:36 -0400

    Fixed Sync Issue Random Number

--- Assets/Scripts/PhotonWaitController.cs
+++ Assets/Scripts/PhotonWaitController.cs
@@ -227,7 +227,10 @@ public class PhotonWaitController : MonoBehaviour
         }
         //TODO Currently Disabled For Alpha Testing To Fix Xross Issues
         //key += "_" + UnityEngine.Random.Range(0, 9999999).ToString();
-
+        int synchronizedSeed = PhotonNetwork.CurrentRoom.Name.GetHashCode() + waitCount;
+        System.Random random = new System.Random(synchronizedSeed);
+        key += "_" + random.Next(0, 9999999).ToString();
+        
         keys.Add(key);
 
         if (!GManager.instance.IsAI)
