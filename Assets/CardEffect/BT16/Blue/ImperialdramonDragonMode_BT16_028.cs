de5e5fb8be4a4f505b73e4bea15de042e040e582
Albert Vasconcellos <albertvasconcellos@live.ca>
2024-06-02 07:20:39 -0400

    Added BT16 namespace to various blue cards

--- Assets/CardEffect/BT16/Blue/ImperialdramonDragonMode_BT16_028.cs
+++ Assets/CardEffect/BT16/Blue/ImperialdramonDragonMode_BT16_028.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace DCGO.CardEffects
+namespace DCGO.CardEffects.BT16
 {
     public class ImperialdramonDragonMode_BT16_028 : CEntity_Effect
     {
