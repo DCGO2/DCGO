40fe6d8f165384c325d53df92cae1a067386f710
mbunch_kascope <michael8818bunch@gmail.com>
2024-06-26 13:43:16 -0400

    fixing a using, that threw build error

--- Assets/CardEffect/EX6/Green/Lopmon_EX6_032.cs
+++ Assets/CardEffect/EX6/Green/Lopmon_EX6_032.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 namespace DCGO.CardEffects.EX6
 {
