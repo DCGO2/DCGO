a8970dfdec0f2d7035b52f060cc061259d223959
JanusClockwork <57453187+JanusClockwork@users.noreply.github.com>
2024-08-23 09:43:42 -0400

    Pushing Otamamon and what I have of Geko.

---
+++ Assets/CardEffect/EX7/Blue/Otamamon_EX7_015.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DCGO.CardEffects
+{
+    public class Otamamon_EX7_015 : CEntity_Effect
+    {
+        public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
+        {
+            List<ICardEffect> cardEffects = new List<ICardEffect>();
+            
+            #region Alternate Digivolution
+            if (timing == EffectTiming.None)
+            {
+                bool PermanentCondition(Permanent targetPermanent)
+                {
+                    return targetPermanent.TopCard.CardTraits.Contains("NSp");
+                }
+
+                cardEffects.Add(CardEffectFactory.AddSelfDigivolutionRequirementStaticEffect(
+                    permanentCondition: PermanentCondition,
+                    digivolutionCost: 0,
+                    ignoreDigivolutionRequirement: false,
+                    card: card,
+                    condition: null)
+                );
+            }
+            #endregion 
+        
+            #region All Turns
+            
+            if (timing == EffectTiming.None)
+            {
+                //Use this instead of activateClass?
+                CannotReduceCostClass cannotReduceCostClass = new CannotReduceCostClass();
+                cannotReduceCostClass.SetUpICardEffect("Players can't reduce play costs", CanUseCondition, card);
+                cannotReduceCostClass.SetUpCannotReduceCostClass(
+                    playerCondition: PlayerCondition,
+                    targetPermanentsCondition: TargetPermanentsCondition,
+                    cardCondition: CardCondition);
+                cardEffects.Add(cannotReduceCostClass);
+                
+                string EffectDiscription()
+                {
+                    return "[All Turns] Players can't reduce play costs.";
+                }
+
+                bool CanUseCondition(Hashtable hashtable)
+                {
+                    return CardEffectCommons.IsExistOnBattleArea(card);
+                }
+
+                bool PlayerCondition(Player player)
+                {
+                    return true;
+                }
+
+                bool TargetPermanentsCondition(List<Permanent> targetPermanents)
+                {
+                    if (targetPermanents == null)
+                    {
+                        return true;
+                    }
+
+                    else
+                    {
+                        if (targetPermanents.Count((permanent) => permanent != null) == 0)
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+
+                bool CardCondition(CardSource cardSource)
+                {
+                    return cardSource.HasPlayCost;
+                }
+            }
+            
+            #endregion
+
+            return cardEffects;
+        }
+    }
+}
