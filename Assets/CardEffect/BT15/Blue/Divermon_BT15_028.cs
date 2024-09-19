7f6d49f1169c5b7b481ab8c01ca81fcb2a2796bb
cchmc-burkewm <William.Burke@cchmc.org>
2024-09-18 21:19:14 -0400

    Updated Cards

--- Assets/CardEffect/BT15/Blue/Divermon_BT15_028.cs
+++ Assets/CardEffect/BT15/Blue/Divermon_BT15_028.cs
@@ -28,10 +28,7 @@ public class Divermon_BT15_028 : CEntity_Effect
             {
                 if (CardEffectCommons.IsPermanentExistsOnOpponentBattleAreaDigimon(permanent, card))
                 {
-                    if (permanent.DigivolutionCards.Count((cardSource) => !cardSource.CanNotTrashFromDigivolutionCards(activateClass)) >= 1)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
 
                 return false;
@@ -41,10 +38,7 @@ public class Divermon_BT15_028 : CEntity_Effect
             {
                 if (CardEffectCommons.IsPermanentExistsOnOpponentBattleAreaDigimon(permanent, card))
                 {
-                    if (permanent.DigivolutionCards.Count >= 1)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
 
                 return false;
--- Assets/CardEffect/BT5/Blue/Hexebraumon_BT5_032.cs
+++ Assets/CardEffect/BT5/Blue/Hexebraumon_BT5_032.cs
@@ -28,10 +28,7 @@ public class Hexebraumon_BT5_032 : CEntity_Effect
             {
                 if (CardEffectCommons.IsPermanentExistsOnOpponentBattleAreaDigimon(permanent, card))
                 {
-                    if (permanent.DigivolutionCards.Count((cardSource) => !cardSource.CanNotTrashFromDigivolutionCards(activateClass)) >= 1)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
 
                 return false;
