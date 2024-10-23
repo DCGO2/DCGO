428e942b66748a31e028fe54d358fa51d963bca4
Joe Delia <joe.delia.cs@gmail.com>
2024-10-23 15:55:49 -0400

    add namespace on zeed

--- Assets/CardEffect/BT19/Red/ZeedMillenniummon_BT19_101.cs
+++ Assets/CardEffect/BT19/Red/ZeedMillenniummon_BT19_101.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace DCGO.CardEffects
+namespace DCGO.CardEffects.BT19
 {
     public class ZeedMillenniummon_BT19_101 : CEntity_Effect
     {
