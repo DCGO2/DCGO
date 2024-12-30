1ca589d82355846b5f89329be9837dfdd02321d7
mbunch_kascope <michael8818bunch@gmail.com>
2024-12-29 22:25:51 -0500

    missing namespaces

--- Assets/CardEffect/EX8/Black/Gotsumon_EX8_046.cs
+++ Assets/CardEffect/EX8/Black/Gotsumon_EX8_046.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace DCGO.CardEffects
+namespace DCGO.CardEffects.EX8
 {
     public class Gotsumon_EX8_046 : CEntity_Effect
     {
