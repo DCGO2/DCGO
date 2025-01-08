a9135361d384d9ec55abd0b2951bac3b34ef9e51
Joe Delia <joe.delia.cs@gmail.com>
2025-01-08 16:42:10 -0500

    Shoeshoemon and self-deleting familiar token

--- Assets/Scripts/ContinuousController.cs
+++ Assets/Scripts/ContinuousController.cs
@@ -110,6 +110,7 @@ public class ContinuousController : MonoBehaviour
     public CEntity_Base GyuukimonToken { get; private set; }
     public CEntity_Base KoHagurumonToken { get; private set; }
     public CEntity_Base FamiliarToken { get; private set; }
+    public CEntity_Base SelfDeleteFamiliarToken { get; private set; }
     public CEntity_Base VoleeZerdruckenToken { get; private set; }
     public CEntity_Base UkaNoMitamaToken { get; private set; }
     public CardRestriction BanList { get; private set; } = new CardRestriction(new List<CardLimitCount>(), new List<BannedPair>());
@@ -265,6 +266,27 @@ public class ContinuousController : MonoBehaviour
         };
 
         await FamiliarToken.GetCardSprite();
+        
+        SelfDeleteFamiliarToken = new CEntity_Base()
+        {
+            cardColors = new List<CardColor>() { CardColor.Yellow },
+            PlayCost = -1,
+            Level = 0,
+            CardName_JPN = "",
+            CardName_ENG = "Familiar",
+            Form_JPN = new List<string>(),
+            Form_ENG = new List<string>(),
+            Attribute_JPN = new List<string>(),
+            Attribute_ENG = new List<string>(),
+            Type_JPN = new List<string>(),
+            Type_ENG = new List<string>(),
+            CardSpriteName = "EX7-030-token",
+            cardKind = CardKind.Digimon,
+            DP = 3000,
+            CardEffectClassName = "SelfDeleteFamiliar_P_165_token"
+        };
+
+        await SelfDeleteFamiliarToken.GetCardSprite();
 
         VoleeZerdruckenToken = new CEntity_Base()
         {
