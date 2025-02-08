910e0157ac52dc4feddf610c64dd9d8385d312b3
mbunch_kascope <michael8818bunch@gmail.com>
2025-02-08 10:03:21 -0500

    update asset files

--- Assets/Editor/Fixing Scripts/MatchClassNames.cs
+++ Assets/Editor/Fixing Scripts/MatchClassNames.cs
@@ -22,128 +22,29 @@ namespace DCGO.Tools.Repair
 
             Debug.Log($"ASSET PATH: {path}");
 
-            List<CEntity_Base> List = GetAsset.LoadAll<CEntity_Base>(path)
-                .OrderBy(x => x.name.Substring(x.name.LastIndexOf("-")+1, 2)).ToList();
+            List<CEntity_Base> List = GetAsset.LoadAll<CEntity_Base>(path).ToList();
 
-            string className = "";
-            string editList = "Asset Name - Original Class - New Class \n";
-            int foundCount = 0;
+            string assetName = "";
 
             //Locate All mismatched classNames
             foreach (CEntity_Base card in List)
             {
-                className = "";
+                assetName = card.CardSpriteName.Replace("-", "_");
 
-                if (String.IsNullOrEmpty(card.CardEffectClassName))
-                {
-                    Debug.Log($"Class Name Empty: {card.name}");
-                    continue;
-                }
-
-                if (!card.CardEffectClassName.Contains(card.CardID.Replace("-", "_")))
-                {
-                    Debug.Log($"Using Alternate Card: {card.name}");
-                    continue;
-                }
-
-                className = FixCharactersInClassName($"{card.CardID}");
-
-                if (className == card.CardEffectClassName)
+                if (assetName == card.name)
                 {
                     Debug.Log($"Matches already: {card.name}");
-                    className = "";
                     continue;
                 }
 
-                editList += $"{card.name} - {card.CardEffectClassName} - {className}\n";
+                Debug.Log($"Name Change: {card.name} - {assetName}");
 
-                if (!classNameDictionary.ContainsKey(className))
-                    classNameDictionary.Add(className, card.CardEffectClassName);
-
-                /*if (!card.name.Contains("_P"))
-                {
-                    if (String.IsNullOrEmpty(card.CardEffectClassName))
-                        continue;
-
-                    if (!card.CardEffectClassName.Contains(card.CardID.Replace("-", "_")))
-                        continue;
-
-                    className = card.CardEffectClassName;
-                    continue;
-                }
-                
-
-                if (!card.CardEffectClassName.Equals(className))
-                {
-                    if (!classNameDictionary.ContainsKey(className))
-                        classNameDictionary.Add(className, card.CardEffectClassName);
-
-                    //Debug.Log($"{card.name}: {card.CardIndex} - {card.CardEffectClassName} != {className}");
-                }*/
-
-                if (!className.IsNullOrEmpty())
-                {
-                    card.CardEffectClassName = className;
-                    AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(card), card.CardSpriteName.Replace("-", "_"));
-                    EditorUtility.SetDirty(card);
-                }
-                
+                AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(card), assetName);
+                EditorUtility.SetDirty(card);
             }
 
-
-
-            /*foreach (string key in classNameDictionary.Keys)
-            {
-                List<CEntity_Base> filtered = List.Filter(x => x.CardEffectClassName == key).ToList();
-
-                foreach (CEntity_Base card in filtered)
-                {
-                    
-                    //card.CardEffectClassName = classNameDictionary[key];
-                    //EditorUtility.SetDirty(card);
-                }
-                    
-
-                Debug.Log($"Filtered Count: {filtered.Count}");
-            }*/
-            //Debug.Log($"{key} - {classNameDictionary[key]}");
-            Debug.Log(editList);
-            Debug.Log($"DONE: found {classNameDictionary.Keys.Count}");
+            Debug.Log($"DONE");
             return;
-
-            string FixCharactersInClassName(string str)
-            {
-                string name = str;
-
-                name = FixCharactersInName(name);
-
-                name = name
-                    .Replace("-", "_")
-                    .Replace(".", "")
-                    .Replace("'", "")
-                    .Replace(",","")
-                    .Replace("&", "And")
-                    .Replace("(", "")
-                    .Replace(")", "");
-
-                return name;
-            }
-
-            //Parse ScriptableObject Name
-            string FixCharactersInName(string str)
-            {
-                string name = str;
-
-                name = name
-                    .Replace(" ", "")
-                    .Replace(":", "")
-                    .Replace("?", "")
-                    .Replace("!", "")
-                    .Replace("<", "")
-                    .Replace(">", "");
-
-                return name;
-            }
         }
     }
 }
