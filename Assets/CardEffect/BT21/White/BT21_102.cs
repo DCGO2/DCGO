86d6244e44ccf096bfeb2f4412b7177a2d1eeaa3
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2025-03-28 17:39:52 +0000

    Update BT21_102.cs

--- Assets/CardEffect/BT21/White/BT21_102.cs
+++ Assets/CardEffect/BT21/White/BT21_102.cs
@@ -57,6 +57,75 @@ namespace DCGO.CardEffects.BT21
 
             if (timing == EffectTiming.OnDeclaration)
             {
+                ActivateClass activateClass = new ActivateClass();
+                activateClass.SetUpICardEffect("Play Adventure/Hero Digimon for reduced cost", CanUseCondition, card);
+                activateClass.SetUpActivateClass(CanActivateCondition, ActivateCoroutine, 1, true, EffectDiscription());
+                activateClass.SetHashString("Play_BT21_102");
+                cardEffects.Add(activateClass);
+
+                string EffectDiscription()
+                    => "[Main] [Once Per Turn] You may play 1 [ADVENTURE]/[Hero] trait card with a play cost of 2 or less from your hand without paying the cost. For each of your Tamers' colors, add 1 to this effect's play cost maximum. Then, return this Tamer to the bottom of the deck.";
+
+                bool CanUseCondition(Hashtable hashtable)
+                    => CardEffectCommons.IsExistOnBattleArea(card)
+                    && CardEffectCommons.IsOwnerTurn(card)
+                    && CardEffectCommons.HasMatchConditionOwnersHand(card, CanSelectDigimon);
+
+                bool CanActivateCondition(Hashtable hashtable)
+                    => CardEffectCommons.IsExistOnBattleArea(card);
+
+                bool CanSelectDigimon(CardSource source)
+                    => source.IsDigimon
+                    && source.BasePlayCostFromEntity <= AdjustedPlayCostMax()
+                    && (source.HasAdventureTraits || source.HasHeroTraits);
+
+                bool IsTamerOnField(Permanent permanent)
+                    => CardEffectCommons.IsPermanentExistsOnOwnerBattleArea(permanent, card)
+                    && permanent.IsTamer;
+
+                int AdjustedPlayCostMax()
+                    => 2 + CardEffectCommons.GetColourCountOnOwnerBattleArea(card, IsTamerOnField);
+
+                IEnumerator ActivateCoroutine(Hashtable hashtable)
+                {
+                    SelectHandEffect selectHandEffect = GManager.instance.GetComponent<SelectHandEffect>();
+                    selectHandEffect.SetUp(
+                        selectPlayer: card.Owner,
+                        canTargetCondition: CanSelectDigimon,
+                        canTargetCondition_ByPreSelecetedList: null,
+                        canEndSelectCondition: null,
+                        maxCount: 1,
+                        canNoSelect: true,
+                        canEndNotMax: false,
+                        isShowOpponent: true,
+                        selectCardCoroutine: SelectCardCoroutine,
+                        afterSelectCardCoroutine: null,
+                        mode: SelectHandEffect.Mode.Custom,
+                        cardEffect: activateClass);
+
+                    selectHandEffect.SetUpCustomMessage("Select 1 digimon to play.",
+                        "The opponent is selecting 1 digimon to play.");
+                    selectHandEffect.SetUpCustomMessage_ShowCard("Played Card");
+
+                    yield return StartCoroutine(selectHandEffect.Activate());
+
+                    IEnumerator SelectCardCoroutine(CardSource cardSource)
+                    {
+                        yield return ContinuousController.instance.StartCoroutine(CardEffectCommons.PlayPermanentCards(
+                            cardSources: new List<CardSource>() { cardSource },
+                            activateClass: activateClass,
+                            payCost: false,
+                            isTapped: false,
+                            root: SelectCardEffect.Root.Hand,
+                            activateETB: true,
+                            isBreedingArea: false
+                        ));
+                    }
+
+                    var cardSources = new List<CardSource>() { card };
+                    yield return ContinuousController.instance.StartCoroutine(CardObjectController.AddLibraryBottomCards(cardSources));
+                    yield return ContinuousController.instance.StartCoroutine(GManager.instance.GetComponent<Effects>().ShowCardEffect2(cardSources, "Deck Bottom Cards", true, true));
+                }
             }
 
             #endregion
