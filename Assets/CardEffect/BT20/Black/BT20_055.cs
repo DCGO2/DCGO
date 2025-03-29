5d5f1889c68990c19a0fc577794c22d9942ec9ac
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2025-03-29 13:17:13 +0000

    updated effect text

--- Assets/CardEffect/BT20/Black/BT20_055.cs
+++ Assets/CardEffect/BT20/Black/BT20_055.cs
@@ -135,7 +135,7 @@ namespace DCGO.CardEffects.BT20
                             mode: SelectPermanentEffect.Mode.Destroy,
                             cardEffect: activateClass);
 
-                        selectPermanentEffect.SetUpCustomMessage("Select 1 Digimon to destroy.", "The opponent is selecting 1 Digimon to destroy.");
+                        selectPermanentEffect.SetUpCustomMessage("Select 1 Digimon to delete.", "The opponent is selecting 1 Digimon to delete.");
 
                         yield return ContinuousController.instance.StartCoroutine(selectPermanentEffect.Activate());
                     }
@@ -238,7 +238,7 @@ namespace DCGO.CardEffects.BT20
                             mode: SelectPermanentEffect.Mode.Destroy,
                             cardEffect: activateClass);
 
-                        selectPermanentEffect.SetUpCustomMessage("Select 1 Digimon to destroy.", "The opponent is selecting 1 Digimon to destroy.");
+                        selectPermanentEffect.SetUpCustomMessage("Select 1 Digimon to delete.", "The opponent is selecting 1 Digimon to delete.");
 
                         yield return ContinuousController.instance.StartCoroutine(selectPermanentEffect.Activate());
                     }
