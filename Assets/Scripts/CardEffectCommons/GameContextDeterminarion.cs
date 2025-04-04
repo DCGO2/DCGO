6e0853272dbf7128af9dccb20161478a64f903e3
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2025-04-04 17:27:06 +0100

    Removed unused method

--- Assets/Scripts/CardEffectCommons/GameContextDeterminarion.cs
+++ Assets/Scripts/CardEffectCommons/GameContextDeterminarion.cs
@@ -526,15 +526,4 @@ public partial class CardEffectCommons
     }
 
     #endregion
-
-    #region Get multi colour count from permanents in owners battle area
-
-    public static int GetColourCountOnOwnerBattleArea(CardSource card, Func<Permanent, bool> canGetCardColour) =>
-        card.Owner.GetBattleAreaPermanents()
-        .Filter(x => canGetCardColour(x))
-        .Select(x => x.TopCard)
-        .SelectMany(x => x.CardColors)
-        .ToList().Count;
-
-    #endregion
 }
