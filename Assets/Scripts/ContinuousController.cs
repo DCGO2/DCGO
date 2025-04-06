f3d7d608f831f850194db5140ac119754843ee4d
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2025-04-06 02:52:21 +0100

    Implemented Petrification Token

--- Assets/Scripts/ContinuousController.cs
+++ Assets/Scripts/ContinuousController.cs
@@ -118,6 +118,7 @@ public class ContinuousController : MonoBehaviour
     public CEntity_Base RapidmonToken { get; private set; }
     public CEntity_Base PipeFoxToken { get; private set; }
     public CEntity_Base AthoRenePorToken { get; private set; }
+    public CEntity_Base PetrificationToken { get; private set; }
     public CardRestriction BanList { get; private set; } = new CardRestriction(new List<CardLimitCount>(), new List<BannedPair>());
 
     void LoadBanList()
@@ -436,6 +437,27 @@ public class ContinuousController : MonoBehaviour
         };
 
         await AthoRenePorToken.GetCardSprite();
+
+        PetrificationToken = new CEntity_Base()
+        {
+            cardColors = new List<CardColor>() { CardColor.White },
+            PlayCost = -1,
+            Level = 0,
+            CardName_JPN = "",
+            CardName_ENG = "Petrification",
+            Form_JPN = new List<string>(),
+            Form_ENG = new List<string>(),
+            Attribute_JPN = new List<string>(),
+            Attribute_ENG = new List<string>(),
+            Type_JPN = new List<string>(),
+            Type_ENG = new List<string>(),
+            CardSpriteName = "BT21-029-token",
+            cardKind = CardKind.Digimon,
+            DP = 3000,
+            CardEffectClassName = "BT21_029_token"
+        };
+
+        await PetrificationToken.GetCardSprite();
     }
 
     public static ContinuousController instance = null;
