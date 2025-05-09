4957d1857f557c2cd0539e07ac5238f5a2476f3a
Andrej Erdelsky <erd.andrej@gmail.com>
2025-05-09 10:32:03 +0200

    Add EX9_016 cardEffect

---
+++ Assets/CardEffect/EX9/Blue/EX9_016.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+
+// Betamon
+namespace DCGO.CardEffects
+{
+    public class EX9_016 : CEntity_Effect
+    {
+        public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
+        {
+            List<ICardEffect> cardEffects = new List<ICardEffect>();
+
+            #region Alternative Digivolution Condition
+
+            if (timing == EffectTiming.None)
+            {
+                bool PermanentCondition(Permanent targetPermanent)
+                {
+                    return targetPermanent.TopCard.IsLevel2 && targetPermanent.TopCard.EqualsTraits("DM");
+                }
+
+                cardEffects.Add(CardEffectFactory.AddSelfDigivolutionRequirementStaticEffect(
+                    permanentCondition: PermanentCondition,
+                    digivolutionCost: 0,
+                    ignoreDigivolutionRequirement: false,
+                    card: card,
+                    condition: null)
+                );
+            }
+
+            #endregion
+            
+            #region Training
+
+            if (timing == EffectTiming.None)
+            {
+                // TODO Implement Training
+            }
+
+            #endregion
+
+            #region Jamming - ESS
+
+            if (timing == EffectTiming.None)
+            {
+                cardEffects.Add(CardEffectFactory.JammingSelfStaticEffect(
+                    isInheritedEffect: true,
+                    card: card,
+                    condition: null));
+            }
+
+            #endregion
+
+            return cardEffects;
+        }
+    }
+}
