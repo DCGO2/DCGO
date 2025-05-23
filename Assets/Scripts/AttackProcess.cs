86f508f2c42c5d9f08adbe020cf75725bdf25e59
mbunch_kascope <michael8818bunch@gmail.com>
2025-05-23 09:30:34 -0400

    fixing tamer attacking for game

--- Assets/Scripts/AttackProcess.cs
+++ Assets/Scripts/AttackProcess.cs
@@ -275,7 +275,7 @@ public class AttackProcess : MonoBehaviourPunCallbacks
                 goto EndAttack;
             }
 
-            if (AttackingPermanent.TopCard == null)
+            if (AttackingPermanent.TopCard == null || !AttackingPermanent.TopCard.IsDigimon)
             {
                 goto EndAttack;
             }
@@ -463,7 +463,7 @@ public class AttackProcess : MonoBehaviourPunCallbacks
         {
             yield return ContinuousController.instance.StartCoroutine(GManager.instance.autoProcessing.StackSkillInfos(effectHashtable, EffectTiming.OnEndAttack));
         }
-
+        
         // activate cutin effects
         yield return ContinuousController.instance.StartCoroutine(GManager.instance.autoProcessing.AutoProcessCheck());
 
@@ -485,7 +485,6 @@ public class AttackProcess : MonoBehaviourPunCallbacks
         SecurityDigimon = null;
         IsAttacking = false;
         IsEndAttack = false;
-        UnityEngine.Debug.Log("ATTACK IS OVER");
     }
 
     public IEnumerator SwitchDefender(ICardEffect cardEffect, bool isBlock, Permanent newDefendingPermanent)
