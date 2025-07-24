63b244049461a63fa6e6551814d72b22829c5769
mbunch_kascope <michael8818bunch@gmail.com>
2025-07-24 14:41:41 -0400

    BT22 fixes

--- Assets/CardEffect/BT14/Red/BT14_017.cs
+++ Assets/CardEffect/BT14/Red/BT14_017.cs
@@ -40,7 +40,7 @@ namespace DCGO.CardEffects.BT14
                     {
                         if (!CardEffectCommons.IsExistOnBreedingAreaDigimon(card))
                         {
-                            if (!cardSource.IsDigiEgg && cardSource.CardDP <= 6000)
+                            if (cardSource.CardDP <= 6000)
                             {
                                 return true;
                             }
