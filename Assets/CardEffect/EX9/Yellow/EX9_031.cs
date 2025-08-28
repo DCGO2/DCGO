10c894e4f73db41d603064631469ba8422f55669
skikklesman <rstewart555@gmail.com>
2025-08-27 22:33:12 -0400

    EX9_031 ESS Fix
    Fixing bug "ESS does not trigger if opponent has no Digimon". Copied implementation from BT22_029.

--- Assets/CardEffect/EX9/Yellow/EX9_031.cs
+++ Assets/CardEffect/EX9/Yellow/EX9_031.cs
@@ -300,7 +300,7 @@ namespace DCGO.CardEffects.EX9
 
                 bool CanActivateCondition(Hashtable hashtable)
                 {
-                    return CardEffectCommons.IsExistOnBattleAreaDigimon(card) && CardEffectCommons.HasMatchConditionPermanent(CanSelectPermanentCondition);
+                    return CardEffectCommons.IsExistOnBattleAreaDigimon(card);
                 }
 
                 IEnumerator ActivateCoroutine(Hashtable hashtable)
