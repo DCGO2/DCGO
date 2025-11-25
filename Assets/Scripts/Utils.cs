865e64d15c23d2835d78733e960f88ab371eea6c
mbunch_kascope <michael8818bunch@gmail.com>
2025-11-24 23:29:08 -0500

    moving script files

--- Assets/Scripts/Utils.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Utils
-{
-    public static string PluralFormSuffix(int count) => count >= 2 ? "s" : "";
-}
-
