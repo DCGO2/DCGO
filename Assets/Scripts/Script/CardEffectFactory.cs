cab80af9beff8a75fe0134a8f6f0d25d138ed28a
hooperk <hooperk@tcd.ie>
2026-03-11 07:24:06 +0000

    Fix CEF method not appearing as a card effect

--- Assets/Scripts/Script/CardEffectFactory.cs
+++ Assets/Scripts/Script/CardEffectFactory.cs
@@ -655,7 +655,8 @@ public partial class CardEffectFactory
 
             yield return ContinuousController.instance.StartCoroutine(new IReduceSecurity(
                 player: card.Owner,
-                refSkillInfos: ref ContinuousController.instance.nullSkillInfos).ReduceSecurity());
+                refSkillInfos: ref ContinuousController.instance.nullSkillInfos,
+                activateClass).ReduceSecurity());
 
             #endregion
         }
@@ -693,7 +694,8 @@ public partial class CardEffectFactory
 
             yield return ContinuousController.instance.StartCoroutine(new IReduceSecurity(
                 player: card.Owner,
-                refSkillInfos: ref ContinuousController.instance.nullSkillInfos).ReduceSecurity());
+                refSkillInfos: ref ContinuousController.instance.nullSkillInfos,
+                activateClass).ReduceSecurity());
 
             #endregion
         }
