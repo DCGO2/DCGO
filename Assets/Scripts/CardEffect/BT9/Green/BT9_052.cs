bc8a581651c133641e3377cfa8e55622699ac72c
x89rt2 <x89rt2@gmail.com>
2026-03-15 18:20:56 -0400

    Update BT9_052.cs

--- Assets/Scripts/CardEffect/BT9/Green/BT9_052.cs
+++ Assets/Scripts/CardEffect/BT9/Green/BT9_052.cs
@@ -35,7 +35,7 @@ public class BT9_052 : CEntity_Effect
 
             string EffectDiscription()
             {
-                return "[When Digivolving] If [Okuwamon] or [X Antibody] is in this DigimonÅ's digivolution cards, suspend 1 of your opponentÅ's Digimon. Then, if this Digimon is attacking, you may switch the target of attack to 1 of your opponentÅ's unsuspended Digimon.";
+                return "[When Digivolving] If [Okuwamon] or [X Antibody] is in this Digimon's digivolution cards, suspend 1 of your opponent's Digimon. Then, if this Digimon is attacking, you may switch the target of attack to 1 of your opponent's unsuspended Digimon.";
             }
 
             bool CanSelectPermanentCondition(Permanent permanent)
