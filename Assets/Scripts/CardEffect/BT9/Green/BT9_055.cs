64faaa1f03b8952070a04d7199b81be3937ee529
x89rt2 <x89rt2@gmail.com>
2026-03-15 18:25:40 -0400

    Update BT9_055.cs

--- Assets/Scripts/CardEffect/BT9/Green/BT9_055.cs
+++ Assets/Scripts/CardEffect/BT9/Green/BT9_055.cs
@@ -159,7 +159,7 @@ public class BT9_055 : CEntity_Effect
 
             string EffectDiscription()
             {
-                return "[End of Attack][Once Per Turn] If [GranKuwagamon] or [X Antibody] is in this Digimon's digivolution cards, suspend 1 of your opponent's Digimon and unsuspend this Digimon.";
+                return "[End of Attack][Once Per Turn] If [GranKuwagamon] or [X Antibody] is in this Digimon's digivolution cards, suspend 1 of your opponent's Digimon and unsuspend this Digimon.";
             }
 
             bool CanSelectPermanentCondition(Permanent permanent)
