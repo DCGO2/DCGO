3888987a4f0a16685fa8f54d8ffed1572db86c6b
x89rt2 <x89rt2@gmail.com>
2026-03-15 17:43:00 -0400

    Update BT12_017.cs

--- Assets/Scripts/CardEffect/BT12/Red/BT12_017.cs
+++ Assets/Scripts/CardEffect/BT12/Red/BT12_017.cs
@@ -25,7 +25,7 @@ namespace DCGO.CardEffects.BT12
 
                 string EffectDiscription()
                 {
-                    return "[When Digivolving] Delete 1 of your opponent's Digimon with 6000 DP or less. If this Digimon has a red Tamer in its digivolution cards, delete 1 of your opponentÅ's Digimon with DP less than or equal to this DigimonÅ instead.";
+                    return "[When Digivolving] Delete 1 of your opponent's Digimon with 6000 DP or less. If this Digimon has a red Tamer in its digivolution cards, delete 1 of your opponent's Digimon with DP less than or equal to this Digimon instead.";
                 }
 
                 bool CanSelectPermanentCondition(Permanent permanent)
