8e7776590650fbc94e2d95922f7ed1d1c6f05337
x89rt2 <x89rt2@gmail.com>
2026-03-15 18:19:12 -0400

    Update BT9_096.cs

--- Assets/Scripts/CardEffect/BT9/Blue/BT9_096.cs
+++ Assets/Scripts/CardEffect/BT9/Blue/BT9_096.cs
@@ -21,7 +21,7 @@ public class BT9_096 : CEntity_Effect
 
             string EffectDiscription()
             {
-                return "[Main] Return 1 of your opponentÅ's level 4 or lower Digimon to its ownerÅ's hand. Then, if you have a Digimon with [Jellymon] in its name or with [Jellymon] in its digivolution cards, return 1 of your opponentÅ's Tamers to its ownerÅ's hand.";
+                return "[Main] Return 1 of your opponent's level 4 or lower Digimon to its owner¬Å's hand. Then, if you have a Digimon with [Jellymon] in its name or with [Jellymon] in its digivolution cards, return 1 of your opponent's Tamers to its owner's hand.";
             }
 
             bool CanSelectPermanentCondition(Permanent permanent)
