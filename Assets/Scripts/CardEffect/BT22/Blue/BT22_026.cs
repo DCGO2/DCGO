eb21b18188a47088868acbdedcac3029a28098a2
x89rt2 <x89rt2@gmail.com>
2026-03-17 16:32:34 -0400

    Update BT22_026.cs

--- Assets/Scripts/CardEffect/BT22/Blue/BT22_026.cs
+++ Assets/Scripts/CardEffect/BT22/Blue/BT22_026.cs
@@ -80,7 +80,7 @@ namespace DCGO.CardEffects.BT22
                             canTargetCondition_ByPreSelecetedList: null,
                             canEndSelectCondition: null,
                             maxCount: 1,
-                            canNoSelect: true,
+                            canNoSelect: false,
                             canEndNotMax: false,
                             selectPermanentCoroutine: SelectPermanentCoroutine,
                             afterSelectPermanentCoroutine: null,
