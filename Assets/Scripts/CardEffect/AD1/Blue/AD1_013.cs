92b26254215ace6bd1579347ecbb10f73a056897
hooperk <hooperk@tcd.ie>
2026-03-18 15:53:07 +0000

    Fix all AD1 Alt Digivolution conditions to use new format

--- Assets/Scripts/CardEffect/AD1/Blue/AD1_013.cs
+++ Assets/Scripts/CardEffect/AD1/Blue/AD1_013.cs
@@ -18,12 +18,11 @@ namespace DCGO.CardEffects.AD1
             {
                 static bool PermanentCondition(Permanent targetPermanent)
                 {
-                    return targetPermanent.TopCard.IsLevel5
-                        && (targetPermanent.TopCard.EqualsTraits("Blue Flare")
-                            || targetPermanent.TopCard.EqualsTraits("Xros Heart"));
+                    return targetPermanent.TopCard.EqualsTraits("Blue Flare")
+                            || targetPermanent.TopCard.EqualsTraits("Xros Heart");
                 }
 
-                cardEffects.Add(CardEffectFactory.AddSelfDigivolutionRequirementStaticEffect(permanentCondition: PermanentCondition, digivolutionCost: 3, ignoreDigivolutionRequirement: false, card: card, condition: null));
+                cardEffects.Add(CardEffectFactory.AddSelfDigivolutionRequirementStaticEffect(level: 5, permanentCondition: PermanentCondition, digivolutionCost: 3, ignoreDigivolutionRequirement: false, card: card, condition: null));
             }
 
             #endregion
