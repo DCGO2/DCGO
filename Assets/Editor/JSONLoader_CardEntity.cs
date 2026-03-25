b9bad399bed94933e0d5b4145f14ae049d3a8e8f
Michael8818 <michael8818bunch@gmail.com>
2026-03-24 20:58:15 -0400

    load banlist comment, remove restriction value

--- Assets/Editor/JSONLoader_CardEntity.cs
+++ Assets/Editor/JSONLoader_CardEntity.cs
@@ -152,7 +152,7 @@ namespace DCGO.CardEntities
             cardEntity.rarity = card.rarity.Equals("-") ? Rarity.None : (Rarity)Enum.Parse(typeof(Rarity), card.rarity);
             cardEntity.OverflowMemory = GetOverflowMemory(card.aceEffect);
             cardEntity.CardID = card.id;
-            cardEntity.MaxCountInDeck = GetMaxCount(card.restrictions.japanese, card.rule);
+            cardEntity.MaxCountInDeck = GetMaxCount(card.rule);
 
             cardEntity.LinkDP = dpParse(card.linkDP);
             cardEntity.LinkEffect = card.linkEffect;
@@ -466,18 +466,12 @@ namespace DCGO.CardEntities
         }
 
         //Parse Max count in deck
-        int GetMaxCount(string restriction, string rule)
+        int GetMaxCount(string rule)
         {
             int value = 4;
 
             if (!String.IsNullOrEmpty(rule))
                 value = GetRuleMaxCount(rule);
-
-            if (restriction == "Banned")
-                value = 0;
-
-            if(restriction.StartsWith("Restricted"))
-                value = int.Parse(restriction.Substring(restriction.Length-1));
 
             return value;
         }
