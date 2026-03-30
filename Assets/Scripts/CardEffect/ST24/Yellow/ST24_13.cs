faca1bd404990e0150f954e26db568876e563088
x89rt2 <x89rt2@gmail.com>
2026-03-30 09:06:12 -0400

    Update ST24_13.cs

--- Assets/Scripts/CardEffect/ST24/Yellow/ST24_13.cs
+++ Assets/Scripts/CardEffect/ST24/Yellow/ST24_13.cs
@@ -10,7 +10,7 @@ namespace DCGO.CardEffects.ST24
         public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
         {
             List<ICardEffect> cardEffects = new List<ICardEffect>();
-            
+
             #region Name Rule
             if (timing == EffectTiming.None)
             {
@@ -18,7 +18,7 @@ namespace DCGO.CardEffects.ST24
                 changeCardNamesClass.SetUpICardEffect("[Rule] Name: Also treated as [Marcus Damon]/[Thomas H. Norstein].", _ => true, card);
                 changeCardNamesClass.SetUpChangeCardNamesClass(changeCardNames: ChangeCardNames);
                 cardEffects.Add(changeCardNamesClass);
-            
+
                 List<string> ChangeCardNames(CardSource cardSource, List<string> cardNames)
                 {
                     if (cardSource == card)
@@ -26,24 +26,26 @@ namespace DCGO.CardEffects.ST24
                         cardNames.Add("Marcus Damon");
                         cardNames.Add("Thomas H. Norstein");
                     }
-            
+
                     return cardNames;
                 }
             }
             #endregion
-            
+
             #region Shared OP/SOMP
             string SharedEffectName = "Place top card of deck face down under this tamer, then if enemy has Digimon gain 1 memory";
 
+            CardEffectFactory.ActivateClassesForSharedEffects
+                (ref cardEffects, timing, card,
+                    SharedEffectName,
+                    SharedActivateCoroutine,
+                    SharedEffectDescription,
+                    optional: false,
+                    onPlay: true,
+                    startOfYourMainPhase: true);
+
             string SharedEffectDescription(string tag) =>
                 $"[{tag}] You may place the top card of your deck face down under this tamer. Then, if your opponent has a Digimon, gain 1 memory.";
-
-            bool SharedCanActivateCondition(Hashtable hashtable)
-            {
-                return CardEffectCommons.IsExistOnBattleArea(card)
-                    && (card.Owner.LibraryCards.Count >= 1
-                        || card.Owner.Enemy.GetBattleAreaDigimons().Count >= 1);
-            }
 
             IEnumerator SharedActivateCoroutine(Hashtable hashtable, ActivateClass activateClass)
             {
@@ -53,7 +55,7 @@ namespace DCGO.CardEffects.ST24
 
                     selectionElements.Add(new(message: $"Yes", value: 1, spriteIndex: 0));
                     selectionElements.Add(new(message: $"No", value: 2, spriteIndex: 1));
-                    
+
                     string selectPlayerMessage = "Will you place the top card from your deck under this Tamer face down?";
                     string notSelectPlayerMessage = "The opponent is choosing whether to place the top card from their deck under their Tamer face down.";
 
@@ -75,43 +77,7 @@ namespace DCGO.CardEffects.ST24
                     yield return ContinuousController.instance.StartCoroutine(card.Owner.AddMemory(1, activateClass));
                 }
             }
-            #endregion
-
-            #region On Play
-            if (timing == EffectTiming.OnEnterFieldAnyone)
-            {
-                ActivateClass activateClass = new ActivateClass();
-                activateClass.SetUpICardEffect(SharedEffectName, CanUseCondition, card);
-                activateClass.SetUpActivateClass(SharedCanActivateCondition,
-                    hash => SharedActivateCoroutine(hash, activateClass), -1, false,
-                    SharedEffectDescription("On Play"));
-                cardEffects.Add(activateClass);
-
-                bool CanUseCondition(Hashtable hashtable)
-                {
-                    return CardEffectCommons.IsExistOnBattleArea(card)
-                        && CardEffectCommons.CanTriggerOnPlay(hashtable, card);
-                }
-            }
-            #endregion
-
-            #region Start of Your Main Phase
-            if (timing == EffectTiming.OnStartMainPhase)
-            {
-                ActivateClass activateClass = new ActivateClass();
-                activateClass.SetUpICardEffect(SharedEffectName, CanUseCondition, card);
-                activateClass.SetUpActivateClass(SharedCanActivateCondition,
-                    hash => SharedActivateCoroutine(hash, activateClass), -1, false,
-                    SharedEffectDescription("Start of Your Main Phase"));
-                cardEffects.Add(activateClass);
-
-                bool CanUseCondition(Hashtable hashtable)
-                {
-                    return CardEffectCommons.IsExistOnBattleArea(card)
-                        && CardEffectCommons.IsOwnerTurn(card);
-                }
-            }
-            #endregion
+            #endregion       
 
             #region All Turns
             if (timing == EffectTiming.OnDigivolutionCardDiscarded)
@@ -130,7 +96,7 @@ namespace DCGO.CardEffects.ST24
                 {
                     return CardEffectCommons.IsExistOnBattleArea(card)
                         && CardEffectCommons.IsOwnerTurn(card)
-                        && //trigger condition goes here;
+                        && CardEffectCommons.CanTriggerOnTrashDigivolutionCard(hashtable, permanent => permanent == card.PermanentOfThisCard(), effect => effect != null, cardSource => true);
                 }
 
                 bool CanActivateCondition(Hashtable hashtable)
@@ -138,7 +104,7 @@ namespace DCGO.CardEffects.ST24
                     return CardEffectCommons.IsExistOnBattleArea(card);
                 }
 
-                bool CanSelectPermanentCondition (Permanent permanent)
+                bool CanSelectPermanentCondition(Permanent permanent)
                 {
                     return CardEffectCommons.IsPermanentExistsOnOwnerBattleAreaDigimon(permanent, card)
                         && permanent.TopCard.EqualsTraits("DATA SQUAD");
