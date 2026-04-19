1cb20a153a40180c0e34039f8c548f992d3ce313
x89rt2 <x89rt2@gmail.com>
2026-04-19 08:47:35 -0400

    Update BT25_063.cs

--- Assets/Scripts/CardEffect/BT25/Black/BT25_063.cs
+++ Assets/Scripts/CardEffect/BT25/Black/BT25_063.cs
@@ -10,6 +10,28 @@ namespace DCGO.CardEffects.BT25
         {
             List<ICardEffect> cardEffects = new List<ICardEffect>();
 
+            #region Alternative Digivolve Condition
+            if (timing == EffectTiming.None)
+            {
+                static bool PermanentCondition(Permanent targetPermanent)
+                {
+                    return targetPermanent.TopCard.EqualsTraits("ACCEL");
+                }
+
+                cardEffects.Add(CardEffectFactory.AddSelfDigivolutionRequirementStaticEffect(PermanentCondition, 0, false, card, null, level: 2));
+            }
+
+            if (timing == EffectTiming.None)
+            {
+                static bool PermanentCondition(Permanent targetPermanent)
+                {
+                    return targetPermanent.TopCard.EqualsCardName("Missimon");
+                }
+
+                cardEffects.Add(CardEffectFactory.AddSelfDigivolutionRequirementStaticEffect(PermanentCondition, 0, false, card, null));
+            }
+            #endregion
+
             #region WM/OP Shared
             string SharedEffectName = "Reveal 3, add [Chaosmon] in name or [D-Brigade]/[ACCEL] trait to hand, return rest to top or bot";
 
@@ -30,7 +52,7 @@ namespace DCGO.CardEffects.BT25
 
             bool CanSelectCardCondition(CardSource cardSource)
             {
-                return cardSource.ContainsCardName("Chaosmon")            
+                return cardSource.ContainsCardName("Chaosmon")
                     || cardSource.EqualsTraits("D-Brigade")
                     || cardSource.EqualsTraits("ACCEL");
             }
