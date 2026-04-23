dcab5583f0d92726bccf1cc3e08e344f9c7682e4
x89rt2 <x89rt2@gmail.com>
2026-04-23 16:05:59 -0400

    Update TurnStateMachine.cs

--- Assets/Scripts/Script/TurnStateMachine.cs
+++ Assets/Scripts/Script/TurnStateMachine.cs
@@ -153,6 +153,8 @@ public class TurnStateMachine : MonoBehaviourPunCallbacks
                         {
                             return "Opponent";
                         }
+
+                        //return playerName;
                     }
                 }
 
