1fa5a6276cc881c5eee30eab588ac5f999ab4e40
x89rt2 <x89rt2@gmail.com>
2026-05-02 12:13:05 -0400

    Update BT25_002.cs
    Missing setisinherited

--- Assets/Scripts/CardEffect/BT25/Blue/BT25_002.cs
+++ Assets/Scripts/CardEffect/BT25/Blue/BT25_002.cs
@@ -17,6 +17,7 @@ namespace DCGO.CardEffects.BT25
                 activateClass.SetUpICardEffect("Both players <Draw 1>", CanUseCondition, card);
                 activateClass.SetUpActivateClass(CanActivateCondition, ActivateCoroutine, 1, false, EffectDescription());
                 activateClass.SetHashString("BT25_002_Inherited");
+                activateClass.SetIsInheritedEffect(true);
                 cardEffects.Add(activateClass);
 
                 string EffectDescription()
