b0138d2baa4749974bbf0f92a9528b6d470e4000
x89rt2 <x89rt2@gmail.com>
2026-05-10 09:56:30 -0400

    Update ST21_04.cs

--- Assets/Scripts/CardEffect/ST21/Blue/ST21_04.cs
+++ Assets/Scripts/CardEffect/ST21/Blue/ST21_04.cs
@@ -366,7 +366,7 @@ namespace DCGO.CardEffects.ST21
                         }
                     }
 
-                    if (Used)
+                    if (!Used)
                     {
                         activateClass.RemoveUse();
                     }
