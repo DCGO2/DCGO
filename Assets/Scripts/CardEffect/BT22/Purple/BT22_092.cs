6a5d98eadef4574854ce8fa5182c669767d156db
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2026-05-12 11:34:08 +0100

    removed if, now always add use

--- Assets/Scripts/CardEffect/BT22/Purple/BT22_092.cs
+++ Assets/Scripts/CardEffect/BT22/Purple/BT22_092.cs
@@ -210,7 +210,7 @@ namespace DCGO.CardEffects.BT22
                                             if (selectedEffect.CanUse(null))
                                             {
                                                 yield return ContinuousController.instance.StartCoroutine(((ActivateICardEffect)selectedEffect).Activate_Optional_Effect_Execute(null));
-                                                if (selectedEffect.MaxCountPerTurn == 1) ((ActivateICardEffect)selectedEffect).AddUse();
+                                                ((ActivateICardEffect)selectedEffect).AddUse();
                                                 yield return ContinuousController.instance.StartCoroutine(card.Owner.AddMemory(1, activateClass));
                                             }
                                         }
