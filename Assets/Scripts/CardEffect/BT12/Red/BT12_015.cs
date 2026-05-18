95c3fcc13eb6e8dc51940ece30bf606abd5fda53
x89rt2 <x89rt2@gmail.com>
2026-05-18 16:46:22 -0400

    Update BT12_015.cs

--- Assets/Scripts/CardEffect/BT12/Red/BT12_015.cs
+++ Assets/Scripts/CardEffect/BT12/Red/BT12_015.cs
@@ -362,12 +362,12 @@ namespace DCGO.CardEffects.BT12
                                                 payCost: true,
                                                 targetPermanent: selectedPermanent,
                                                 isTapped: false,
-                                                failedProcess: OnFail(),
+                                                failedProcess: OnFail(card),
                                                 root: SelectCardEffect.Root.Hand,
-                                                activateETB: true).PlayCard());              
+                                                activateETB: true).PlayCard());
                                         }
 
-                                        IEnumerator OnFail()
+                                        IEnumerator OnFail(CardSource cardSource)
                                         {
                                             yield return ContinuousController.instance.StartCoroutine(CardObjectController.AddTrashCard(card));
                                         }
