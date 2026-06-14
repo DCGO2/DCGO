cfb8060b330029bb0f9789306986f28a0d54ebf7
x89rt2 <x89rt2@gmail.com>
2026-06-14 14:45:39 -0400

    Main effects not OPT

--- Assets/Scripts/CardEffect/EX12/Red/EX12_013.cs
+++ Assets/Scripts/CardEffect/EX12/Red/EX12_013.cs
@@ -94,6 +94,8 @@ namespace DCGO.CardEffects.EX12
 
                     if (selectedCard != null)
                     {
+                        isUsed = true;
+
                         #region Reduce Cost
                         IEnumerator ReduceCost(string type)
                         {
