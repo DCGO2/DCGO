49e564e5c8cefbf15ec0cb88b92e9e44a73dc775
x89rt2 <x89rt2@gmail.com>
2026-06-18 16:35:45 -0400

    Remove unnecessary stuff

--- Assets/Scripts/CardEffect/EX12/Blue/EX12_034.cs
+++ Assets/Scripts/CardEffect/EX12/Blue/EX12_034.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 // Erlangmon
 namespace DCGO.CardEffects.EX12
