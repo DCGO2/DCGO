4234c9d47996c73e7fb54ff63564abaf28b3ebbe
x89rt2 <x89rt2@gmail.com>
2026-06-24 08:56:00 -0400

    P_113 and EX11_003

--- Assets/Scripts/CardEffect/EX11/Green/EX11_003.cs
+++ Assets/Scripts/CardEffect/EX11/Green/EX11_003.cs
@@ -11,17 +11,16 @@ namespace DCGO.CardEffects.EX11
             List<ICardEffect> cardEffects = new List<ICardEffect>();
 
             #region Inherited
-
             if (timing == EffectTiming.OnAddSecurity)
             {
                 ActivateClass activateClass = new ActivateClass();
                 activateClass.SetUpICardEffect("Draw 1.", CanUseCondition, card);
-                activateClass.SetUpActivateClass(CanActivateCondition, ActivateCoroutine, 1, false, EffectDiscription());
+                activateClass.SetUpActivateClass(CanActivateCondition, ActivateCoroutine, 1, false, EffectDescription());
                 activateClass.SetHashString("EX11_003_ESS");
                 activateClass.SetIsInheritedEffect(true);
                 cardEffects.Add(activateClass);
 
-                string EffectDiscription()
+                string EffectDescription()
                 {
                     return "[Your Turn] [Once Per Turn] When face-up [Royal Base] trait cards are placed in your security stack, <Draw 1>.";
                 }
@@ -30,16 +29,14 @@ namespace DCGO.CardEffects.EX11
                 {
                     List<CardSource> securityCards = CardEffectCommons.GetCardSourcesFromHashtable(hashtable).Filter(SecurityCondition);
 
-                    return CardEffectCommons.IsExistOnBattleArea(card)
+                    return CardEffectCommons.IsExistOnBattleAreaDigimonTrigger(card, activateClass)
                         && CardEffectCommons.IsOwnerTurn(card)
-                        && CardEffectCommons.CanTriggerWhenAddSecurity(hashtable, player => player == card.Owner)
-                        && securityCards.Count > 0;
+                        && CardEffectCommons.CanTriggerWhenAddSecurity(hashtable, player => player == card.Owner);
                 }
 
                 bool CanActivateCondition(Hashtable hashtable)
                 {
-                    return CardEffectCommons.IsExistOnBattleArea(card)
-                        && card.Owner.LibraryCards.Count >= 1;
+                    return CardEffectCommons.IsExistOnBattleAreaDigimonActivate(card, activateClass);
                 }
 
                 bool SecurityCondition(CardSource cardSource)
@@ -57,7 +54,6 @@ namespace DCGO.CardEffects.EX11
                     );
                 }
             }
-
             #endregion
 
             return cardEffects;
