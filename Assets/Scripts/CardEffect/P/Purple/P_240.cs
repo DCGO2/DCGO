d2e9c9b4a4fc656a89e5c480e4ddc600efe75129
x89rt2 <x89rt2@gmail.com>
2026-07-01 22:09:53 -0400

    Update P_240.cs

--- Assets/Scripts/CardEffect/P/Purple/P_240.cs
+++ Assets/Scripts/CardEffect/P/Purple/P_240.cs
@@ -293,11 +293,11 @@ namespace DCGO.CardEffects.P
                         #region Setup Location Selection
                         if (canSelectHand && canSelectTrash)
                         {
-                            List<SelectionElement<int>> selectionElements = new List<SelectionElement<int>>();
+                            List<SelectionElement<int>> selectionElements = new List<SelectionElement<int>>
                             {
-                                new SelectionElement<int>(message: $"From hand", value: 1, spriteIndex: 0);
-                                new SelectionElement<int>(message: $"From trash", value: 2, spriteIndex: 0);
-                                new SelectionElement<int>(message: $"Don't play", value: 3, spriteIndex: 1);
+                                new(message: $"From hand", value: 1, spriteIndex: 0),
+                                new(message: $"From trash", value: 2, spriteIndex: 0),
+                                new(message: $"Don't play", value: 3, spriteIndex: 1)
                             };
 
                             string selectPlayerMessage = "From which area do you select a card?";
