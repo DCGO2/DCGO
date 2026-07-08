341f1b84c1ab0940c6ea9094ba721c1238fda31d
x89rt2 <x89rt2@gmail.com>
2026-07-07 20:02:38 -0400

    Fix including cardname

--- Assets/Scripts/CardEffect/EX4/White/EX4_072.cs
+++ Assets/Scripts/CardEffect/EX4/White/EX4_072.cs
@@ -25,14 +25,20 @@ namespace DCGO.CardEffects.EX4
                     return true;
                 }
 
-                List<string> changeCardNames(CardSource cardSource, List<string> CardNames)
+                List<string> changeCardNames(CardSource cardSource, List<string> cardNames)
                 {
                     if (cardSource == card)
                     {
-                        CardNames.Add("Plug-In_EX4_072");
+                        for (int i = 0; i < cardNames.Count; i++)
+                        {
+                            if (!cardNames[i].Contains("Plug-In"))
+                            {
+                                cardNames[i] = $"Plug-In {cardNames[i]}";
+                            }
+                        }
                     }
 
-                    return CardNames;
+                    return cardNames;
                 }
             }
             #endregion
