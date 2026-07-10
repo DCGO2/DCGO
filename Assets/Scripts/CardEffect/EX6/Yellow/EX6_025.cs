2efc96351018ff39f91c98b66a7403b78c629dc2
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2026-07-10 23:12:03 +0100

    Update EX6_025.cs

--- Assets/Scripts/CardEffect/EX6/Yellow/EX6_025.cs
+++ Assets/Scripts/CardEffect/EX6/Yellow/EX6_025.cs
@@ -81,7 +81,7 @@ namespace DCGO.CardEffects.EX6
             bool IsShakamon(CardSource cardSource) => cardSource.EqualsCardName("Shakamon");
 
             bool CanSelectSecMinusPermanentSharedCondition(Permanent permanent)
-                => CardEffectCommons.IsPermanentExistsOnBattleArea(permanent) && permanent.IsDigimon;
+                => CardEffectCommons.IsPermanentExistsOnField(permanent) && permanent.IsDigimon;
 
             #endregion
 
