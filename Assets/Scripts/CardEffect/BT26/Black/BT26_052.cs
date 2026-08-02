7b9811a18c185dcdf6f08fd9102c7a37216b0127
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2026-08-02 17:09:34 +0100

    BT26-052 Pristimon

---
+++ Assets/Scripts/CardEffect/BT26/Black/BT26_052.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+
+// Pristimon
+namespace DCGO.CardEffects
+{
+    public class BT26_052 : CEntity_Effect
+    {
+        public override List<ICardEffect> CardEffects(EffectTiming timing, CardSource card)
+        {
+            List<ICardEffect> cardEffects = new List<ICardEffect>();
+
+            #region On Play
+            if (timing == EffectTiming.OnEnterFieldAnyone)
+            {
+                ActivateClass activateClass = new ActivateClass();
+                activateClass.SetUpICardEffect("Reveal top 3, add 1 [Glowing Dawn] card & 1 black [BEATBREAK] to hand. bottom deck the rest", CanUseCondition, card);
+                activateClass.SetUpActivateClass(CanActivateCondition, ActivateCoroutine, -1, false, EffectDiscription());
+                cardEffects.Add(activateClass);
+
+                string EffectDiscription()
+                {
+                    return "[On Play] Reveal the top 3 cards of your deck. Add 1 card with the [Glowing Dawn] trait and 1 black card with the [BEATBREAK] trait among them to the hand. Return the rest to the bottom of the deck.";
+                }
+
+                bool CanUseCondition(Hashtable hashtable)
+                {
+                    return CardEffectCommons.IsExistOnBattleAreaDigimon(card)
+                        && CardEffectCommons.CanTriggerOnPlay(hashtable, card);
+                }
+
+                bool CanActivateCondition(Hashtable hashtable)
+                {
+                    return CardEffectCommons.IsExistOnBattleAreaDigimon(card);
+                }
+
+                bool CanSelectGlowingDawnTraitCardCondition(CardSource cardSource)
+                {
+                    return cardSource.HasGlowingDawnTraits;
+                }
+
+                bool CanSelectBlackBeatBreakTraitCardCondition(CardSource cardSource)
+                {
+                    return cardSource.HasCardColor(CardColor.Black) && cardSource.HasBeatBreakTraits;
+                }
+
+                IEnumerator ActivateCoroutine(Hashtable hashtable)
+                {
+                    yield return ContinuousController.instance.StartCoroutine(CardEffectCommons.SimplifiedRevealDeckTopCardsAndSelect(
+                        revealCount: 3,
+                        simplifiedSelectCardConditions:
+                        new SimplifiedSelectCardConditionClass[]
+                        {
+                            new SimplifiedSelectCardConditionClass(
+                                canTargetCondition:CanSelectGlowingDawnTraitCardCondition,
+                                message: "Select 1 Glowing Dawn Trait card.",
+                                mode: SelectCardEffect.Mode.AddHand,
+                                maxCount: 1,
+                                selectCardCoroutine: null),
+                            new SimplifiedSelectCardConditionClass(
+                                canTargetCondition:CanSelectBlackBeatBreakTraitCardCondition,
+                                message: "Select 1 Black [BEATBREAK] Trait card.",
+                                mode: SelectCardEffect.Mode.AddHand,
+                                maxCount: 1,
+                                selectCardCoroutine: null),
+                        },
+                        remainingCardsPlace: RemainingCardsPlace.DeckBottom,
+                        mutualConditions: true,
+                        activateClass: activateClass
+                    ));
+                }
+            }
+
+            #endregion
+
+            #region Reboot
+            if (timing == EffectTiming.None)
+            {
+                cardEffects.Add(CardEffectFactory.RebootSelfStaticEffect(isInheritedEffect: true, card: card, condition: null));
+            }
+            #endregion
+
+            return cardEffects;
+        }
+    }
+}
