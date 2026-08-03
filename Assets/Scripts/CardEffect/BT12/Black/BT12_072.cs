d09bb5e0c567fc5eb66cc27727be554107264fa8
x89rt2 <x89rt2@gmail.com>
2026-08-03 16:04:11 -0400

    Revert "Revert "First attempt at solving issues with copy effects""

--- Assets/Scripts/CardEffect/BT12/Black/BT12_072.cs
+++ Assets/Scripts/CardEffect/BT12/Black/BT12_072.cs
@@ -132,62 +132,11 @@ namespace DCGO.CardEffects.BT12
                 }
             }
 
-            if (timing == EffectTiming.None)
-            {
-                AddSkillClass addSkillClass = new AddSkillClass();
-                addSkillClass.SetUpICardEffect("This Digimon gains all effects of [Machinedramon] and [Chaosdramon] in digivolution cards", CanUseCondition, card);
-                addSkillClass.SetUpAddSkillClass(cardSourceCondition: CardSourceCondition, getEffects: GetEffects);
+            #region Copy effects of Machinedramon and Chaosdramon in digivolution cards
+            bool CopyCardCondition(CardSource cardSource) => cardSource.EqualsCardName("Machinedramon") || cardSource.EqualsCardName("Chaosdramon");
 
-                cardEffects.Add(addSkillClass);
-
-                bool CanUseCondition(Hashtable hashtable)
-                {
-                    return CardEffectCommons.IsExistOnBattleArea(card);
-                }
-
-                bool PermanentCondition(Permanent permanent)
-                {
-                    return permanent == card.PermanentOfThisCard();
-                }
-
-                bool CardSourceCondition(CardSource cardSource)
-                {
-                    if (PermanentCondition(cardSource.PermanentOfThisCard()))
-                    {
-                        if (cardSource == cardSource.PermanentOfThisCard().TopCard)
-                        {
-                            return true;
-                        }
-                    }
-
-                    return false;
-                }
-
-                List<ICardEffect> GetEffects(CardSource cardSource, List<ICardEffect> cardEffects, EffectTiming _timing)
-                {
-                    if (CardSourceCondition(cardSource))
-                    {
-                        foreach (CardSource cardSource1 in cardSource.PermanentOfThisCard().DigivolutionCards)
-                        {
-                            if (!cardSource1.IsFlipped)
-                            {
-                                if (cardSource1.EqualsCardName("Machinedramon") || cardSource1.EqualsCardName("Chaosdramon"))
-                                {
-                                    foreach (ICardEffect cardEffect in cardSource1.cEntity_EffectController.GetCardEffects_ExceptAddedEffects(_timing, card))
-                                    {
-                                        if (!cardEffect.IsSecurityEffect && !cardEffect.IsInheritedEffect)
-                                        {
-                                            cardEffects.Add(cardEffect);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    return cardEffects;
-                }
-            }
+            CardEffectFactory.CopyDigivolutionCardEffects(ref cardEffects, timing, card, cardCondition: CopyCardCondition);
+            #endregion
 
             if (timing == EffectTiming.WhenPermanentWouldBeDeleted)
             {
