2e054e950a752a4204181d89f33acf1496f10338
Alysson Correia <alysson.correia@gmail.com>
2026-08-20 19:55:05 -0300

    BT26_068: track isUsed and call RemoveUse() on the All Turns block when the cost is declined

--- Assets/Scripts/CardEffect/BT26/Purple/BT26_068.cs
+++ Assets/Scripts/CardEffect/BT26/Purple/BT26_068.cs
@@ -76,6 +76,8 @@ namespace DCGO.CardEffects.BT26
 
                 IEnumerator ActivateCoroutine(Hashtable hashtable)
                 {
+                    bool isUsed = false;
+
                     if (card.Owner.HandCards.Count >= 1)
                     {
                         CardSource selectedCardToTrash = null;
@@ -108,6 +110,8 @@ namespace DCGO.CardEffects.BT26
 
                         if (selectedCardToTrash != null)
                         {
+                            isUsed = true;
+
                             yield return ContinuousController.instance.StartCoroutine(CardEffectCommons.TrashHandAndProcessAccordingToResult(
                                 player: card.Owner,
                                 hashtable: hashtable,
@@ -141,6 +145,8 @@ namespace DCGO.CardEffects.BT26
                             }
                         }
                     }
+
+                    if (!isUsed) activateClass.RemoveUse();
                 }
             }
             #endregion
