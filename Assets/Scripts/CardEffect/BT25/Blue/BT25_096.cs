ad2e8edf0c5a5eb0e5fd927c6cf294bc7376a0c0
x89rt2 <x89rt2@gmail.com>
2026-08-23 11:56:32 -0400

    Update BT25_096.cs

--- Assets/Scripts/CardEffect/BT25/Blue/BT25_096.cs
+++ Assets/Scripts/CardEffect/BT25/Blue/BT25_096.cs
@@ -329,9 +329,10 @@ namespace DCGO.CardEffects.BT25
                                     payCost: false,
                                     reduceCostTuple: null,
                                     fixedCostTuple: null,
-                                    ignoreDigivolutionRequirementFixedCost: 1,
+                                    ignoreDigivolutionRequirementFixedCost: -1,
                                     isHand: true,
                                     activateClass: activateClass,
+                                    ignoreRequirements: CardEffectCommons.IgnoreRequirement.All,
                                     successProcess: null));
                             }
                         }
